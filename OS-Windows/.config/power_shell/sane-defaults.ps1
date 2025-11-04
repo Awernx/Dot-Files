f8890cb7d8c3c0c9f3c1cb307724e3a68484a80a
Awernx <chander@live.com>
2025-11-04 11:03:28 -0600

    Updated settings in all apps

--- OS-Windows/.config/power_shell/sane-defaults.ps1
+++ OS-Windows/.config/power_shell/sane-defaults.ps1
@@ -9,19 +9,15 @@ $SecondsPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advance
 Set-ItemProperty -Path $SecondsPath -Name "ShowSecondsInSystemClock" -Type DWord -Value "00000001"
 
 #######################################################################
-# Add Context Menu to Windows Explorer to 
-# open files & folders with VS Code
+# Add Context Menu to Windows Explorer to
+# open files & folders with Zed Editor
 #######################################################################
-$VSCodeExe = "$env:LOCALAPPDATA\Programs\Microsoft VS Code\Code.exe"
-
-if (-not (Test-Path $VSCodeExe)) {
-    $VSCodeExe = "$env:ProgramFiles\Microsoft VS Code\Code.exe"
-}
+$ZedExe = "$env:LOCALAPPDATA\Programs\Scoop\apps\zed\current\Zed.exe"
 
 '*','Folder' | ForEach-Object {
     $Path = 'HKCU:\Software\Classes\{0}\shell\pintohome' -f $_
     $null = New-Item -Path "$Path\command" -Force
-    Set-ItemProperty -LiteralPath $Path -Name 'MUIVerb' -Value 'Open with VS Code'
-    Set-ItemProperty -LiteralPath "$Path\command" -Name '(Default)' -Value ('"{0}" "%1"' -f $VSCodeExe)
+    Set-ItemProperty -LiteralPath $Path -Name 'MUIVerb' -Value 'Open with Zed'
+    Set-ItemProperty -LiteralPath "$Path\command" -Name '(Default)' -Value ('"{0}" "%1"' -f $ZedExe)
     Remove-ItemProperty -LiteralPath "$Path\command" -Name "DelegateExecute" -ErrorAction SilentlyContinue
 }
